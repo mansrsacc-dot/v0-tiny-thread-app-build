075d1fcb36a7ebfe056014b5352a27d603702352
mansrsacc-dot <karvelsm@gmail.com>
2026-05-28 01:29:08 -0700

    Remove price from add another design button

--- app/page.tsx
+++ app/page.tsx
@@ -3310,7 +3310,7 @@ export default function TinyThreadStudio() {
                         : "border-[#3e92cc]/60 text-[#3e92cc] hover:border-[#3e92cc] hover:bg-[#3e92cc]/10"
                     )}
                   >
-                    + {t.addAnotherDesign} {addPrice > 0 ? `(+€${addPrice})` : ""}
+                    + {t.addAnotherDesign}
                   </button>
                 );
               })()}
